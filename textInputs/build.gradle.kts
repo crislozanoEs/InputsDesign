32305081dfc03ddf3f9cd30c4fd4833fb85dd4d5
Cristian Lozano <lozanoespitiacris-94@outlook.com>
2024-08-05 18:13:59 -0500

    added maven config

--- textInputs/build.gradle.kts
+++ textInputs/build.gradle.kts
@@ -57,7 +57,6 @@ publishing {
             credentials {
                 // Can be used
                 // githubProperties.getProperty("username")
-                //"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"
                 // System.getenv("GITHUB_USERNAME")
                 username = System.getenv("GITHUB_USERNAME")
                 password = System.getenv("GITHUB_TOKEN")
